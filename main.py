34db64a9458c3ab7c04a2a64a46c414646bff2a8
avmoran <avmoran@wustl.edu>
2018-01-04 13:37:16 -0600

    Added register/login pages; did not add User class yet.

--- main.py
+++ main.py
@@ -16,6 +16,14 @@ class Task(db.Model):
         self.name = name
         self.completed = False
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 @app.route('/', methods=['GET','POST'])
 def index():
 
